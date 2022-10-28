ca2b89b029139f3452eefb88704585f872ba59ea
Sergio Medeiros <sergiof11@outlook.com.br>
2022-10-28 18:19:29 -0300

    Getting the id of each of the movies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./global.css";
 import Home from "./pages/home";
+import Details from "./pages/Details";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/details/:id" element={<Details />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,18 @@
 import { Container, MovieList, Movie } from "./styles";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [movies, setMovies] = useState([]);
   const image_path = "https://image.tmdb.org/t/p/w500";
 
   useEffect(() => {
-    const APIkey = `minha chave aqui`;
+    const APIkey = `9df28ca54fd5020e3c28d0deee455d82`;
     fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${APIkey}&language=en-US&page=1`
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.results);
         setMovies(data.results);
       });
   }, []);
@@ -24,12 +24,13 @@ function Home() {
         {movies.map((movie) => {
           return (
             <Movie key={movie.id}>
-              <a href="https://google.com.br">
+              <Link to={`/details/${movie.id}`}>
                 <img
                   src={`${image_path}${movie.poster_path}`}
                   alt={movie.title}
                 />
-              </a>
+              </Link>
+
               <span>{movie.title}</span>
             </Movie>
           );
